d0beda0d04794cf44187e5d75c19129f3f1da084
Twan H <huiskenstwan@gmail.com>
2020-04-29 12:36:12 +0200

    Update account serializer to include realations

--- tudget/accounts/serializers.py
+++ tudget/accounts/serializers.py
@@ -1,13 +1,16 @@
 from rest_framework import serializers
 from accounts.models import Account
 
+from transactions.serializers import ExpenseSerializer, IncomeSerializer
+
 
 class AccountSerializer(serializers.ModelSerializer):
     """ Serializer for the Account model. """
     # Set the relation w/ the transaction model
-    transactions = serializers.RelatedField(source='transactions.Transaction', many=True, read_only=True)
+    expense_set = ExpenseSerializer(many=True)
+    income_set = IncomeSerializer(many=True)
 
     class Meta:
         model = Account
-        fields = ['name', 'description', 'balance', 'active']
-        read_only_fields = ['_createdOn']
+        fields = ['pk', 'name', 'description', 'balance', 'active', '_createdOn', 'expense_set', 'income_set']
+        read_only_fields = ['_createdOn', 'income_set', 'expense_set']
