9b512d0de74e7735a5458920a8b25c7f63f70601
Twan H <huiskenstwan@gmail.com>
2020-04-29 16:36:44 +0200

    Add api for the savings accounts

--- tudget/savings/urls.py
+++ tudget/savings/urls.py
@@ -1,5 +1,11 @@
 from django.conf.urls import url
-
+from .views import ListAllSavingAccountsView, UpdateSavingsAccountView, DeleteSavingAccountView
 urlpatterns = [
-
+    # api/savings/
+    url(r'^$', ListAllSavingAccountsView.as_view(), name="saving-accounts-all"),
+    # api/savings/<pk>/
+    url(r'^(?P<pk>\d+)/$', UpdateSavingsAccountView.as_view(), name='update-savings-account'),
+    # api/savings/<pk>/delete/
+    url(r'^(?P<pk>\d+)/delete/$', DeleteSavingAccountView.as_view(), name='delete-savings-account'),
 ]
+
--- tudget/savings/views.py
+++ tudget/savings/views.py
@@ -5,18 +5,47 @@ from .models import SavingsAccount
 from .serializers import SavingsAccountSerializer
 
 
-class ListAllSavingAccounts(generics.ListCreateAPIView):
+class ListAllSavingAccountsView(generics.ListCreateAPIView):
     """
     List all the saving accounts, and add the ability to create a new savings account
     url: api/savings/
-
+    methods:
+        - get: Get all the savings accounts
+        - post: Create new savings account
     """
     queryset = SavingsAccount.objects.filter(active=True)
     serializer_class = SavingsAccountSerializer
 
-class UpdateSavingsAccount(generics.UpdateAPIView):
+
+class UpdateSavingsAccountView(generics.UpdateAPIView):
+    """
+    Update an savings account with specific primary key (pk)
+    url: api/savings/<pk>/
+    methods:
+        - patch
+        - puts
+    """
 
     queryset = SavingsAccount.objects.filter(active=True)
     serializer_class = SavingsAccountSerializer
 
-class Dele
+
+class DeleteSavingAccountView(generics.RetrieveAPIView):
+    """
+    Delete a savings accoonts with specific primary key (pk)
+    url: api/savings/<pk>/delete/
+    methods:
+        - get
+    """
+
+    queryset = SavingsAccount.objects.filter(active=True)
+    serializer_class = SavingsAccountSerializer
+
+    # Override the get method.
+    def get(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.active = False
+        instance.save()
+
+        # Return 200.
+        return Response(status=status.HTTP_200_OK)
