ae9d974a7d02dbbafb5cdf22271e3a7ae3064794
Twan H <huiskenstwan@gmail.com>
2020-05-02 12:56:07 +0200

    Add proper data formatting

--- frontend/src/components/TransacionsList.js
+++ frontend/src/components/TransacionsList.js
@@ -29,7 +29,7 @@ function TransactionsList({ transactions, accounts, titleType }) {
               <Box fontSize={2} fontFamily='heading' color='secondary'>{transaction.name}</Box>
               <Box fontSize={0} fontFamily='monospace' justifySelf='end' color={transaction.type === 'expense' ? 'negative' : 'positive'}>{transaction.type === 'expense' && '-'}&euro;{transaction.amount}</Box>
               <Box fontSize={1}>{accounts.filter(accnt => (accnt.pk === transaction.account))[0].name}</Box>
-              <Box fontSize={0} justifySelf='end' color='gray'>{transaction.spendOn}</Box>
+              <Box fontSize={0} justifySelf='end' color='gray'>{new Date(transaction.spendOn).toLocaleDateString('nl-NL')}</Box>
             </Box>
           </Box>
 
