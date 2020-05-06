2947444470944af108c5a5e5eec2067897a79344
Twan H <huiskenstwan@gmail.com>
2020-05-06 18:01:58 +0200

    refactor: auto export fetchAllTags

--- frontend/src/redux/fetchers/index.js
+++ frontend/src/redux/fetchers/index.js
@@ -2,11 +2,11 @@ import { fetchAllAccounts } from "./accounts";
 import { fetchAllBudgets } from "./budgets";
 import { fetchAllTransactions } from "./transactions";
 import { fetchAllCategories } from './categories'
-
-
+import { fetchAllTags } from './tags'
 export {
   fetchAllAccounts,
   fetchAllBudgets,
   fetchAllTransactions,
   fetchAllCategories,
+  fetchAllTags,
 }
