e090299b89ba2d56bac406c5df683b5bef662751
Twan H <huiskenstwan@gmail.com>
2020-05-06 17:15:44 +0200

    feat(fetchers): create categories fetcher to fetch all categories

---
+++ frontend/src/redux/fetchers/categories.js
@@ -0,0 +1,15 @@
+import { fetchAllCategoriesError, fetchAllCategoriesPending, fetchAllCategoriesSuccess } from "../actions/categories"
+
+import { fetchAllCategories as allCategoriesFetcher } from "../../utils/api/categories";
+
+export function fetchAllCategories() {
+  return dispatch => {
+    dispatch(fetchAllCategoriesPending)
+    allCategoriesFetcher
+      .then(resp => resp.json())
+      .then(jsn => {
+        dispatch(fetchAllCategoriesSuccess(jsn))
+      })
+      .catch(err => dispatch(fetchAllCategoriesError(err)))
+  }
+}
