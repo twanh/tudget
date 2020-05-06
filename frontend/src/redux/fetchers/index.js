f52e52ee5a5d970a75ba3d47e24fe45867624857
Twan H <huiskenstwan@gmail.com>
2020-05-06 17:21:29 +0200

    feat(redux/fetchers): Export fetchAllCategories from index.js for easy importing

--- frontend/src/redux/fetchers/index.js
+++ frontend/src/redux/fetchers/index.js
@@ -1,9 +1,12 @@
 import { fetchAllAccounts } from "./accounts";
 import { fetchAllBudgets } from "./budgets";
 import { fetchAllTransactions } from "./transactions";
+import { fetchAllCategories } from './categories'
+
 
 export {
   fetchAllAccounts,
   fetchAllBudgets,
-  fetchAllTransactions
+  fetchAllTransactions,
+  fetchAllCategories,
 }
