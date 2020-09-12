ddb8529d05d79158561c5ae303d4069bcc5b5d23
Twan H <huiskenstwan@gmail.com>
2020-09-12 16:47:38 +0200

    feat(frontend/Accounts): Start work on (all) accounts page

--- tudget-frontend/src/views/Accounts.vue
+++ tudget-frontend/src/views/Accounts.vue
@@ -1,10 +1,34 @@
 <template>
-  <h1>All Accounts</h1>
+  <div>
+    <h2 class="subtitle has-text-weight-light is-size-4 is-family-secondary">All accounts:</h2>
+    <div v-if="!accountsPending">
+      <accounts-bar :accounts="allAccounts" />
+    </div>
+    <div v-else>Loading...</div>
+  </div>
 </template>
 
-
 <script>
+import { mapGetters } from "vuex";
+
+import AccountsBar from "@/components/AccountsBar";
+
 export default {
-  name: "Accounts"
+  name: "Accounts",
+  components: {
+    "accounts-bar": AccountsBar
+  },
+  mounted() {
+    if (this.allAccounts.length === 0) {
+      this.$store.dispatch("accounts/getAllAccounts");
+      console.log("loading");
+    }
+  },
+  computed: {
+    ...mapGetters({
+      allAccounts: "accounts/allAccounts",
+      accountsPending: "accounts/isPending"
+    })
+  }
 };
 </script>
