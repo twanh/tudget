a89446c3a9bb1def10ffc72636597d1418684468
Twan H <huiskenstwan@gmail.com>
2020-09-12 13:40:36 +0200

    feat(frontend/AccountsBar): Create the accounts bar component

---
+++ tudget-frontend/src/components/AccountsBar.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="size ml-3">
+    <div class="columns is-6">
+      <div
+        v-for="account in accounts"
+        class="column account-box"
+        :class="isActive(account.pk) && 'accountIsActive '"
+        :key="account.pk"
+        @click.prevent="handleClick(account.pk)"
+      >
+        <span class="title is-6">{{account.name}}</span>
+        <br />
+        <span class="subtitle is-6">&euro;{{account.balance}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AccountsBar",
+  props: ["accounts"],
+  methods: {
+    handleClick(pk) {
+      this.$router.history.push(`/account/${pk}`);
+    },
+    isActive(pk) {
+      console.log(pk, this.$route);
+      console.log(this.$route.params.pk);
+      if (this.$route.params.pk == pk) {
+        console.log("rtue");
+        return true;
+      }
+      return false;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import "../assets/scss/settings.scss";
+.size {
+  //* Have this determined by an prop?
+  width: 50%;
+  max-width: 100%;
+}
+
+@media only screen and (max-width: $tablet) {
+  .size {
+    width: 100%;
+  }
+}
+
+.account-box {
+  padding: 0.3rem !important;
+  cursor: pointer;
+  background: $backgroundHighlight;
+  box-shadow: 2px 3px 0px 0px rgba(255, 255, 255, 0.125);
+  min-width: 100px;
+  // width: 110px;
+  // max-width: 10%;
+  margin-right: 0.75rem;
+  &:hover {
+    box-shadow: 3px 3px 1px 0px $backgroundAccent;
+  }
+  span {
+    padding: 0;
+  }
+}
+
+.accountIsActive {
+  background-color: $secondary !important;
+  span {
+    color: $background !important;
+  }
+}
+</style>
--- tudget-frontend/src/views/Dashboard.vue
+++ tudget-frontend/src/views/Dashboard.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
     <h2 class="subtitle has-text-weight-light is-size-4 is-family-secondary">Dashboard</h2>
+    <h5 class="subtitle has-text-weight-light is-size-6">All your accounts:</h5>
+    <accounts-bar
+      :accounts="[{pk: 0, name: 'Account #1', balance: '500'},{pk: 1,name: 'Account #2', balance: '60'},{pk: 2,name: 'Account #3', balance: '10000'},{pk: 3,name: 'Account #4', balance: '-200'}]"
+    />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import AccountsBar from "@/components/AccountsBar";
 
 export default {
-  name: "Home"
+  name: "Dashboard",
+  components: {
+    "accounts-bar": AccountsBar
+  }
 };
 </script>
