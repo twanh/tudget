197357631e9e8376b6ff080e30ad37a5f698a022
Twan H <huiskenstwan@gmail.com>
2020-09-13 19:03:23 +0200

    fix: Remove old dependency

--- tudget-frontend/src/main.js
+++ tudget-frontend/src/main.js
@@ -5,12 +5,10 @@ import store from "./store";
 
 import "@/assets/scss/styles.scss";
 import Buefy from "buefy";
-import TrendChart from "vue-trend-chart";
 
 Vue.config.productionTip = false;
 
 Vue.use(Buefy);
-Vue.use(TrendChart);
 
 new Vue({
   router,
