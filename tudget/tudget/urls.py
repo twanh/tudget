1cd50e59ef1e1668d93828d18b42652bb4da933a
Twan H <huiskenstwan@gmail.com>
2020-09-16 13:34:07 +0200

    refactor: Remove redoc docs and change swagger url to `docs/`

--- tudget/tudget/urls.py
+++ tudget/tudget/urls.py
@@ -44,10 +44,8 @@ urlpatterns = [
     # Documentatien routes
     url(r'^swagger(?P<format>\.json|\.yaml)$',
         schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'^swagger/$', schema_view.with_ui('swagger',
-                                           cache_timeout=0), name='schema-swagger-ui'),
-    url(r'^redoc/$', schema_view.with_ui('redoc',
-                                         cache_timeout=0), name='schema-redoc'),
+    url(r'^docs/$', schema_view.with_ui('swagger',
+                                        cache_timeout=0), name='schema-swagger-ui'),
     # Admin routes
     path('admin/', admin.site.urls),
     # api/accounts/
